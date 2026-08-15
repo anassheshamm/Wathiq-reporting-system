a7d4b1dfa8ea19eb1c079c7d0faf5362691cb17e
Anas Hesham <anassheshamm@gmail.com>
2026-08-16 00:38:51 +0300

    fix: configure cors for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use(compression());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
